411cf0a4284b524ed80f5fb21addb199c6a1a590
Nico Montoya <nicomontoya@Nicos-MacBook-Pro.local>
2025-01-26 20:07:06 -0700

    Adjust to fix MIME

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/modern_portfolio_website_eszipy/',
-  server: {
-    hmr: true
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   }
 })
